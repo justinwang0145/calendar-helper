a26376de960a6d7fc816dd30a1103e1ce8983158
Huadi Wang <wanghuadi0145@gmail.com>
2023-05-16 21:28:22 -0700

    feat: add two mock pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <React.Suspense fallback={null}>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Auth />} />
+        </Routes>
+      </React.Suspense>
+    </Router>
   );
 }
 
